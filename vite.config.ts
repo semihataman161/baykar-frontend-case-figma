211fd4764ba4b5e86b892f2781366462426a8908
semih <semih@uraltelekom.com>
2024-06-29 02:47:48 +0300

    Added alias to access elements from relative path.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import autoprefixer from 'autoprefixer'
-import tailwindcss from "tailwindcss";
+import tailwindcss from "tailwindcss"
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +10,11 @@ export default defineConfig({
   css: {
     postcss: {
       plugins: [tailwindcss(), autoprefixer],
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 })
