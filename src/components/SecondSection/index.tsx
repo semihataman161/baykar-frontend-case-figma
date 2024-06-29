b49e4a69ad167c34e70f09d59b4e8b2b3d51e284
semih <semih@uraltelekom.com>
2024-06-29 03:03:43 +0300

    Putted images on top of each other in SecondSection component.

--- src/components/SecondSection/index.tsx
+++ src/components/SecondSection/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import NibhViverra from '@/assets/images/nibhViverra.svg';
 import NibhViverraBackground from '@/assets/images/nibhViverraBackground.svg';
 import CursusAmet from '@/assets/images/cursusAmet.svg';
@@ -11,14 +12,18 @@ const SecondSection = () => {
             <div className="container mx-auto px-4 md:px-8">
                 <div className="flex flex-wrap -mx-4">
                     <div className="w-full md:w-1/3 px-4 mb-8 md:mb-0">
-                        <img
-                            src={NibhViverra}
-                            alt="nibhViverra"
-                        />
-                        <img
-                            src={NibhViverraBackground}
-                            alt="nibhViverraBackground"
-                        />
+                        <div className="relative">
+                            <img
+                                src={NibhViverraBackground}
+                                alt="nibhViverraBackground"
+                                className="absolute top-0 left-[28px]"
+                            />
+                            <img
+                                src={NibhViverra}
+                                alt="nibhViverra"
+                                className="relative"
+                            />
+                        </div>
                         <h3 className="text-[16px] leading-[17.6px] font-medium mb-2">Nibh viverra</h3>
                         <p className="text-[18px] leading-[28.8px]">
                             Sit bibendum donec dolor fames neque vulputate non sit aliquam.
@@ -26,14 +31,18 @@ const SecondSection = () => {
                         </p>
                     </div>
                     <div className="w-full md:w-1/3 px-4 mb-8 md:mb-0">
-                        <img
-                            src={CursusAmet}
-                            alt="cursusAmet"
-                        />
-                        <img
-                            src={CursusAmetBackground}
-                            alt="cursusAmetBackground"
-                        />
+                        <div className="relative">
+                            <img
+                                src={CursusAmetBackground}
+                                alt="cursusAmetBackground"
+                                className="absolute top-0 left-[24px]"
+                            />
+                            <img
+                                src={CursusAmet}
+                                alt="cursusAmet"
+                                className="relative"
+                            />
+                        </div>
                         <h3 className="text-[16px] leading-[17.6px] font-medium mb-2">Cursus amet</h3>
                         <p className="text-[18px] leading-[28.8px]">
                             Sit bibendum donec dolor fames neque vulputate non sit aliquam.
@@ -41,14 +50,18 @@ const SecondSection = () => {
                         </p>
                     </div>
                     <div className="w-full md:w-1/3 px-4 mb-8 md:mb-0">
-                        <img
-                            src={IpsumFermentum}
-                            alt="ipsumFermentum"
-                        />
-                        <img
-                            src={IpsumFermentumBackground}
-                            alt="ipsumFermentumBackground"
-                        />
+                        <div className="relative">
+                            <img
+                                src={IpsumFermentumBackground}
+                                alt="ipsumFermentumBackground"
+                                className="absolute top-0 left-[44px]"
+                            />
+                            <img
+                                src={IpsumFermentum}
+                                alt="ipsumFermentum"
+                                className="relative"
+                            />
+                        </div>
                         <h3 className="text-[16px] leading-[17.6px] font-medium mb-2">Ipsum fermentum</h3>
                         <p className="text-[18px] leading-[28.8px]">
                             Sit bibendum donec dolor fames neque vulputate non sit aliquam.
