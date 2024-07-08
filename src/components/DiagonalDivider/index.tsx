2f34356a2d692ef4fc1ab68cbc2a374f1ed95f26
Semih Ataman <semih@uraltelekom.com>
2024-07-08 19:20:32 +0300

    Made skew-y value of diagonal divider responsive.

--- src/components/DiagonalDivider/index.tsx
+++ src/components/DiagonalDivider/index.tsx
@@ -4,7 +4,7 @@ const DiagonalDivider: React.FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <div className="relative bg-[#FEF3C7] overflow-hidden">
       <div className="absolute inset-0 overflow-hidden">
-        <div className="absolute inset-y-0 left-0 w-full h-full bg-[#FFFBEB] transform origin-left -skew-y-[20deg]"></div>
+        <div className="absolute inset-y-0 left-0 w-full h-full bg-[#FFFBEB] transform origin-left -skew-y-[75deg] md:-skew-y-[20deg]"></div>
       </div>
       <div className="relative z-10">
         {children}
