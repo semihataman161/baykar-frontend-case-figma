ed8ba8138e78809874ded871c32da3a659d4ac9a
Semih Ataman <semih@uraltelekom.com>
2024-07-08 18:22:02 +0300

    Made first section responsive.

--- src/components/FirstSection/index.tsx
+++ src/components/FirstSection/index.tsx
@@ -8,20 +8,20 @@ const FirstSection = () => {
                 <div className="flex flex-col md:flex-row items-center justify-between">
                     <div className="flex flex-col gap-5 w-full md:w-1/2 mb-10 md:mb-0">
                         <div>
-                            <h1 className="text-[72px] leading-[79.2px] font-[800] mb-4">
+                            <h1 className="text-[72px] leading-[79.2px] font-[800] mb-4 text-center md:text-left">
                                 Collectible Sneakers
                             </h1>
                         </div>
                         <div>
-                            <p className="text-[18px] leading-[28.8px] mb-4">
+                            <p className="text-[18px] leading-[28.8px] mb-4 text-center md:text-left">
                                 Sit elit feugiat turpis sed integer integer accumsan turpis. Sed
-                                suspendisse nec lorem mauris. Pharetra, eu imperdiet ipsum ultrices
+                                suspendisse nec lorem mauris. Pharetra eu imperdiet ipsum ultrices
                                 amet.
                             </p>
                         </div>
                         <div>
-                            <div className="flex items-center space-x-4">
-                                <button className="border-2 border-solid border-[#78350F] text-[#78350F] rounded-lg px-[29px] py-3 text-[20px] leading-[24px] tracking-[.5px] font-medium">
+                            <div className="flex items-center space-x-4 justify-center md:justify-start">
+                                <button className="border-2 border-solid border-[#78350F] text-[#78350F] rounded-lg px-[20px] md:px-[29px] py-3 text-[18px] leading-[24px] tracking-[.5px] font-medium">
                                     Sign up now
                                 </button>
                                 <button className="flex items-center gap-2 text-[#78350F] text-[16px] leading-[24px] tracking-[.5px] font-medium px-4 py-2">
@@ -38,6 +38,7 @@ const FirstSection = () => {
                         <img
                             src={Sneakers}
                             alt="sneakers"
+                            className="w-full md:w-[80%] mx-auto"
                         />
                     </div>
                 </div>
